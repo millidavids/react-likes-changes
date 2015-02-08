a6d4fe69517864226d569ae7af9baa75510192fc
David Yurek <david.a.yurek@gmail.com>
2015-02-08 16:08:21 -0500

    some more grunt tasks and directory structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,57 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    sass: {
+      dist: {
+        options: {
+          'sourcemap': 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: './src/assets/styles/sass/',
+          src: ['*.scss'],
+          dest: './src/assets/styles/css/',
+          ext: '.css'
+        }]
+      }
+    },
+
     cssmin: {
       combine: {
         files: {
-          './build/assets/styles/application.min.css': ['./src/assets/styles/*.css']
+          './build/assets/styles/main.min.css': ['./src/assets/styles/css/*.css']
         }
       }
     },
+
+    react: {
+      dynamic_mappings: {
+        files: [{
+          expand: true,
+          cwd: './src/assets/scripts/jsx/',
+          src: ['*.jsx'],
+          dest: './src/assets/scripts/js/',
+          ext: '.js'
+        }]
+      }
+    },
+
     uglify: {
       options: {
         manage: false
       },
       target: {
         files: {
-          './build/assets/scripts/application.min.js': ['./src/assets/scripts/js/*.js']
+          './build/assets/scripts/main.min.js': ['./src/assets/scripts/js/*.js']
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-react');
+
+  grunt.registerTask('default', ['sass', 'cssmin', 'react', 'uglify']);
 };
