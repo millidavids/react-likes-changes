812287c61103e21de7f5695d16e8882507aa430f
David Yurek <david.a.yurek@gmail.com>
2015-02-08 15:28:08 -0500

    uglify js no manage

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,24 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     cssmin: {
-      my_target: {
-        files: [{
-          expand: true,
-          cwd: './app/assets/styles/',
-          src: ['*.css', '!*.min.css'],
-          dest: './app/assets/styles/',
-          ext: '.min.css'
-        }]
+      combine: {
+        files: {
+          './build/assets/styles/application.min.css': ['./src/assets/styles/*.css']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        manage: false
+      },
+      target: {
+        files: {
+          './build/assets/scripts/application.min.js': ['./src/assets/scripts/js/*.js']
+        }
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
