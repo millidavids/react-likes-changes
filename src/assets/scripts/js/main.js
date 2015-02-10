e766cb4f39163a1425dc6c0822095d2ae32fd056
David Yurek <david.a.yurek@gmail.com>
2015-02-10 16:43:58 -0500

    begin the writing

--- src/assets/scripts/js/main.js
+++ src/assets/scripts/js/main.js
@@ -1,2 +1,50 @@
 var React = require('react');
-var Router = require('react-router');
+var $ = require('jquery-browserify');
+
+var LikesAndChanges = React.createClass({displayName: "LikesAndChanges",
+  render: function() {
+    return (
+      React.createElement("div", {className: "likesAndChanges"}, 
+        React.createElement("h2", null, "Likes and Changes"), 
+        React.createElement("hr", null), 
+        React.createElement(AddLikeOrChange, null), 
+        React.createElement(Likes, null), 
+        React.createElement(Changes, null)
+      )
+    );
+  }
+});
+
+var AddLikeOrChange = React.createClass({displayName: "AddLikeOrChange",
+  render: function() {
+    return (
+      React.createElement("div", {className: "addLikeOrChange"}
+      )
+    );
+  }
+});
+
+var Likes = React.createClass({displayName: "Likes",
+  render: function() {
+    return (
+      React.createElement("div", {className: "likes col-xs-6"}, 
+        React.createElement("h3", null, "Likes")
+      )
+    )
+  }
+});
+
+var Changes = React.createClass({displayName: "Changes",
+  render: function() {
+    return (
+      React.createElement("div", {className: "changes col-xs-6"}, 
+        React.createElement("h3", null, "Changes")
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(LikesAndChanges, null),
+  document.body
+);
--- src/assets/scripts/jsx/main.jsx
+++ src/assets/scripts/jsx/main.jsx
@@ -1,2 +1,50 @@
 var React = require('react');
-var Router = require('react-router');
+var $ = require('jquery-browserify');
+
+var LikesAndChanges = React.createClass({
+  render: function() {
+    return (
+      <div className='likesAndChanges'>
+        <h2>Likes and Changes</h2>
+        <hr/>
+        <AddLikeOrChange />
+        <Likes />
+        <Changes />
+      </div>
+    );
+  }
+});
+
+var AddLikeOrChange = React.createClass({
+  render: function() {
+    return (
+      <div className='addLikeOrChange'>
+      </div>
+    );
+  }
+});
+
+var Likes = React.createClass({
+  render: function() {
+    return (
+      <div className='likes col-xs-6'>
+        <h3>Likes</h3>
+      </div>
+    )
+  }
+});
+
+var Changes = React.createClass({
+  render: function() {
+    return (
+      <div className='changes col-xs-6'>
+        <h3>Changes</h3>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <LikesAndChanges />,
+  document.body
+);
